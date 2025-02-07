11a1c0001ef51ab547e13d90dea9cefdbd735e97
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-07 23:12:44 +0100

    feat: update

--- src/modules/components/hello-world/hello-world.tsx
+++ src/modules/components/hello-world/hello-world.tsx
@@ -10,7 +10,9 @@ export const HelloWorld: React.FC<HelloWorldProps> = () => {
         <h1 className='text-2xl font-bold text-gray-800 mb-4'>Hello World!</h1>
         <img className='w-16 h-16 mx-auto' src={SvgAsset} alt='Hello World SVG' />
         <p className='text-sm font-bold text-gray-500 mt-4'>React TS + Vite + Tailwind</p>
-        <p className='text-sm font-bold text-gray-300 mt-2'>Template by <a href='https://github.com/marcoslafoz'>@marcoslafoz</a></p>
+        <p className='text-sm font-bold text-gray-300 mt-2'>
+          Template by <a href='https://github.com/marcoslafoz'>@marcoslafoz</a>
+        </p>
       </div>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000
-  },
   plugins: [
     react(),
     VitePWA({
