e05fc2440b3cb9d395243a78450a276bc599c6bf
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-11 00:47:43 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    './node_modules/flowbite-react/lib/esm/**/*.js',
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
