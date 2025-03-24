1950dc417508be156a6006862be9a1350bb5ba4b
marcoslafoz <marcos.lafoz96@gmail.com>
2025-03-24 14:09:07 +0100

    .

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,10 +1,22 @@
-import { RouterProvider } from 'react-router'
-import { router } from '../common/router'
+import React from 'react'
+import { SvgAsset } from '../common/constants/assets'
+import { ComponentePrueba } from './components/componente-ejemplo'
+
 
 export const App: React.FC = () => {
   return (
     <main>
-      <RouterProvider router={router} />
+      <div className='flex items-center justify-center min-h-screen bg-gray-200'>
+        <div className='text-center p-6 bg-white shadow-lg rounded-xl'>
+          <h1 className='text-2xl font-bold text-gray-800 mb-4'>Hello World!</h1>
+          <img className='w-16 h-16 mx-auto' src={SvgAsset} alt='Hello World SVG' />
+          <p className='text-sm font-bold text-gray-500 mt-4'>React TS + Vite + Tailwind</p>
+          <p className='text-sm font-bold text-gray-300 mt-2'>
+            Template by <a href='https://github.com/marcoslafoz'>@marcoslafoz</a>
+          </p>
+          <ComponentePrueba/>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/modules/components/componente-ejemplo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './estilos.css'
+
+export const ComponentePrueba : React.FC = () => {
+  return (
+    <div className='componente-prueba' >Esto es un componente de prueba</div>
+  )
+}
